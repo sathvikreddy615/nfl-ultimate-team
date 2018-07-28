0891b577a1f344c4e7fc6781a043f024d631f0a4
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-07-28 18:58:46 -0500

    got video feature for login and register working

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import APIManager from "../APIManager";
+import "./Register.css";
+
 
 export default class Register extends Component {
   state = {
@@ -33,51 +35,75 @@ export default class Register extends Component {
   render() {
     return (
       <React.Fragment>
-        <form
-            onSubmit={this.handleRegister}
-            id="registerForm"
-        >
-          {/* register field for name */}
-          <label htmlFor="name">Name:</label>
-          <input
-            onChange={this.handleFieldChange}
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Marcus Mariota"
-          />
 
-          {/* register field for username */}
-          <label htmlFor="username">Username:</label>
-          <input
-            onChange={this.handleFieldChange}
-            type="text"
-            id="username"
-            name="username"
-            placeholder="marcusmariota8"
-          />
+        <video preload="auto" loop="true" autoplay="autoplay" muted id="registerVideo">
+          <source src="https://s3.amazonaws.com/nfl-api-images/videos/Madden+19+Tennessee+Titans+Uniforms.mp4" type="video/mp4" />
+        </video>
 
-          {/* register field for password */}
-          <label htmlFor="password">Password:</label>
-          <input
-            onChange={this.handleFieldChange}
-            type="password"
-            id="password"
-            name="password"
-          />
+        <div className="content">
+          <form
+              onSubmit={this.handleRegister}
+              id="registerForm"
+          >
+            {/* register field for name */}
+            <label htmlFor="name">Name:</label>
 
-          {/* register button */}
-          <button type="submit" id="registerBtn">
-            Register
-          </button>
-        </form>
+            <br/>
 
-        <div id="backToLoginBtn">
-          <button type="button" id="signUpBtn">
-            <Link to={{ pathname: "/login" }}>
-                Back to Login
-            </Link>
-          </button>
+            <input
+              onChange={this.handleFieldChange}
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Marcus Mariota"
+            />
+
+            <br/>
+
+            {/* register field for username */}
+            <label htmlFor="username">Username:</label>
+
+            <br/>
+
+            <input
+              onChange={this.handleFieldChange}
+              type="text"
+              id="username"
+              name="username"
+              placeholder="marcusmariota8"
+            />
+
+            <br/>
+
+            {/* register field for password */}
+            <label htmlFor="password">Password:</label>
+
+            <br/>
+
+            <input
+              onChange={this.handleFieldChange}
+              type="password"
+              id="password"
+              name="password"
+            />
+
+            <br/>
+
+            <br/>
+
+            {/* register button */}
+            <button className="bd-tw-button button is-info is-small is-focused is-rounded is-fullwidth" type="submit" id="registerBtn">
+              Register
+            </button>
+          </form>
+
+          <div id="backToLoginBtn">
+            <button className="bd-tw-button button is-danger is-small is-focused is-rounded" type="button" id="signUpBtn">
+              <Link to={{ pathname: "/login" }}>
+                  Back to Login
+              </Link>
+            </button>
+          </div>
         </div>
       </React.Fragment>
     );
