64786eaa9a278d8ce602a8e0c73101629e65c3ce
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-08-03 11:22:44 -0500

    successfully moved simulation function to ApplicationViews. tested and everything seems to be working the same

--- src/NUT/NUT.js
+++ src/NUT/NUT.js
@@ -11,71 +11,20 @@ export default class NUT extends Component {
     state = {
         userTeam: this.props.userPlayers,
         computerTeam: this.props.computerPlayers,
-        userSum: 0,
-        computerSum: 0,
-        gameResult: "",
         open: false
     };
 
-    // positionsArray = ["QB", "RB", "WR", "TE", "DL", "LB", "DB", "K"];
-
-    // generateComputerPlayers = () => {
-    //     this.positionsArray.forEach(position => {
-    //         APIManager.getPlayersByPosition(position)
-    //         .then(playerArray => {
-    //             let random = playerArray[Math.floor(Math.random() * playerArray.length)];
-    //             let newArray = this.state.computerTeam;
-    //             newArray.push(random);
-    //             console.log(newArray);
-    //             this.setState({computerTeam: newArray})
-    //         })
-    //     })
-    // }
-
-    // componentDidMount = () => {
-    //     return this.props.generateComputerPlayers();
-    //     // console.log(this.state.computerTeam);
-    // }
-
-    sum = (a, b) => {
-        return a + b;
-    }
-
     onOpenModal = () => {
         this.setState({ open: true });
-      };
+    };
 
     onCloseModal = () => {
         this.setState({ open: false });
     };
 
-    simulation = () => {
-        // calculates total user points
-        let userArray = [];
-        this.state.userTeam.forEach(playerObjects => {
-            userArray.push(playerObjects.points);
-        })
-        let sumOfUserArray = userArray.reduce(this.sum);
-        this.setState({userSum: sumOfUserArray});
-
-        // calculates total computer points
-        let computerArray = [];
-        this.state.computerTeam.forEach(playerObjects => {
-            computerArray.push(playerObjects.points);
-        })
-        let sumOfComputerArray = computerArray.reduce(this.sum);
-        this.setState({computerSum: sumOfComputerArray})
-
-        // checks if user won, lost or tied
-        if (sumOfUserArray > sumOfComputerArray) {
-            this.setState({gameResult: "You Won!"});
-        } else if (sumOfUserArray < sumOfComputerArray) {
-            this.setState({gameResult: "You Lost!"});
-        } else if (sumOfUserArray === sumOfComputerArray) {
-            this.setState({gameResult: "You Tied!"});
-        }
-
-        this.onOpenModal(); // modal pops up with results
+    runSimulation = () => {
+        this.props.simulation();
+        this.onOpenModal();
     }
 
   render() {
@@ -85,7 +34,7 @@ export default class NUT extends Component {
 
           <div id="simulateBtnContainer">
                     <button
-                        onClick={this.simulation}
+                        onClick={this.runSimulation}
                         id="simulateBtn"
                         className="bd-tw-button button is-danger is-focused is-rounded is-fullwidth is-outlined"
                         type="button">
@@ -96,7 +45,7 @@ export default class NUT extends Component {
         {/* Containers for the User */}
 
         <div id="userContainer">
-            <div id="userName"><i>Sathvik Reddy</i>: {this.state.userSum} pts.</div>
+            <div id="userName"><i>Sathvik Reddy</i>: {this.props.userSum} pts.</div>
         </div>
         <div id="userCardsContainer" className="columns">
         {this.state.userTeam.map(player => (
@@ -142,7 +91,7 @@ export default class NUT extends Component {
           ))}
         </div>
         <div id="computerContainer">
-            <div id="computerName"><i>Computer</i>: {this.state.computerSum} pts.</div>
+            <div id="computerName"><i>Computer</i>: {this.props.computerSum} pts.</div>
         </div>
 
         {/* ------------------ */}
@@ -153,9 +102,9 @@ export default class NUT extends Component {
 
         <div id="gary">
             <Modal open={open} onClose={this.onCloseModal} center>
-                <h1>{this.state.gameResult}</h1>
+                <h1>{this.props.gameResult}</h1>
                 <br/>
-                <h2>{this.state.userSum} - {this.state.computerSum}</h2>
+                <h2>{this.props.userSum} - {this.props.computerSum}</h2>
                 {/* <h2>{this.state.computerSum}</h2> */}
                 <div id="modalBtnContainer">
                    <Link to="/buildteam">
